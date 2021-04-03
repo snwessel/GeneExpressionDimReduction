3c479282778fb275933fb9e2b5d180baa312772a
snwessel <wessel.s@husky.neu.edu>
2021-04-03 12:15:25 -0400

    Switch to using numpy instead of pandas

--- app/data_loader.py
+++ app/data_loader.py
@@ -1,6 +1,6 @@
 import csv
 import os 
-import pandas as pd
+import numpy as np
 
 class DataLoader:
   def __init__(self):
@@ -9,19 +9,21 @@ class DataLoader:
   def load_data_files(self):
     """Load the reads from each of the normalized_results files in the data directory.
       Return a dataframe containing the file contents"""
-    df = pd.DataFrame(columns=[])
+    X = np.empty((20532,0), int)
     # iterate through the files
     path = ".\\data\\TCGA-GBM\\legacy\\Gene_expression\\Gene_expression_quantification"
     for dirname in os.listdir(path):
       for filename in os.listdir(path + "\\" + dirname):
+        # read from the tsv file
         tsv_file = open(path + "\\" + dirname + "\\" + filename)
         reader = csv.reader(tsv_file, delimiter="\t")
         gene_exp_values = []
         for row in reader:
           gene_exp_values.append(row[1])
-        df[filename] = gene_exp_values
+        # append to our matrix
+        X = np.append(X, np.array([gene_exp_values]).T, axis=1)
 
-    print("# files:", len(df.columns))
-    return df
+    print("Created a matrix with shape: ", X)
+    return X
 
   
