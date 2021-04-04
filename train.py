b2bf46ba9132f2d2c772cc61f6c11687def12778
snwessel <wessel.s@husky.neu.edu>
2021-04-04 00:17:34 -0400

    implement t-sne and pca

--- train.py
+++ train.py
@@ -1,4 +1,29 @@
 from app import data_loader
 
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+# load gene expression data as a numpy array
+print("Loading data...")
 dl = data_loader.DataLoader()
-print(dl.load_data_files())
+X = dl.load_data_files()
+
+# embed using t-SNE
+print("Reducing dimensions...")
+X_embedded = TSNE(n_components=2).fit_transform(X)
+print("Reduced from", X.shape, "to", X_embedded.shape)
+
+# embed using PCA
+pca = PCA(n_components = 2)
+fit = pca.fit(X.T)
+pca_components = fit.components_
+
+plt.plot(X_embedded.T[0], X_embedded.T[1], 'ro')
+plt.title('t-SNE')
+plt.show()
+
+print("Used PCA to reduce to:", pca_components.shape)
+plt.plot(pca_components[0], pca_components[1], 'ro')
+plt.title('PCA')
+plt.show()
