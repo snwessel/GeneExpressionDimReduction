1154b4a6dad4b67d65e1b98ccc122bc8385e2ad3
snwessel <wessel.s@husky.neu.edu>
2021-04-04 00:17:55 -0400

    Debug data loading

--- app/data_loader.py
+++ app/data_loader.py
@@ -9,7 +9,7 @@ class DataLoader:
   def load_data_files(self):
     """Load the reads from each of the normalized_results files in the data directory.
       Return a dataframe containing the file contents"""
-    X = np.empty((20532,0), int)
+    X = np.empty((0, 20531), int)
     # iterate through the files
     path = ".\\data\\TCGA-GBM\\legacy\\Gene_expression\\Gene_expression_quantification"
     for dirname in os.listdir(path):
@@ -17,13 +17,13 @@ class DataLoader:
         # read from the tsv file
         tsv_file = open(path + "\\" + dirname + "\\" + filename)
         reader = csv.reader(tsv_file, delimiter="\t")
+        next(reader)
         gene_exp_values = []
         for row in reader:
-          gene_exp_values.append(row[1])
-        # append to our matrix
-        X = np.append(X, np.array([gene_exp_values]).T, axis=1)
+          gene_exp_values.append(float(row[1]))
+        # append as a row to our matrix
+        X = np.append(X, np.array([gene_exp_values]), axis=0)
 
-    print("Created a matrix with shape: ", X)
     return X
 
   
