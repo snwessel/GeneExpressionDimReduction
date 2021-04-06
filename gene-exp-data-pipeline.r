94abbc62dc77a7d1bc5b78899db5a0342705d838
snwessel <wessel.s@husky.neu.edu>
2021-04-05 21:27:37 -0400

    Download multiple projects data at once

--- gene-exp-data-pipeline.r
+++ gene-exp-data-pipeline.r
@@ -1,34 +1,39 @@
-# Download gene expression data from a legacy GDC database
-# Documentation for GDCQuery here: https://rdrr.io/bioc/TCGAbiolinks/man/GDCquery.html 
-query <- GDCquery(project = "TCGA-LAML", # Required, iterate through projects to get more
-                  data.category = "Transcriptome Profiling",
-                  data.type = "Gene Expression Quantification",
-                  workflow.type = "HTSeq - FPKM",
-                  #sample.type = "Primary Blood Derived Cancer - Bone Marrow", 
-                  experimental.strategy = "RNA-Seq")
+# iterate through the projects we want to query
+projects <- list("TCGA-KIRC", "TCGA-HNSC", "TCGA-LAML")
+for (project_name in projects) {
+  
+  # Download gene expression data from a legacy GDC database
+  # Documentation for GDCQuery here: https://rdrr.io/bioc/TCGAbiolinks/man/GDCquery.html 
+  query <- GDCquery(project = project_name, # Required, iterate through projects to get more
+                    data.category = "Transcriptome Profiling",
+                    data.type = "Gene Expression Quantification",
+                    workflow.type = "HTSeq - FPKM",
+                    #sample.type = "Primary Blood Derived Cancer - Bone Marrow", 
+                    experimental.strategy = "RNA-Seq")
+  
+  # Download files (about 546MB of data)
+  data_path = paste("C:/Users/Sarah/Desktop/Repos/GeneExpressionDimReduction/data/", project_name, sep="")
+  GDCdownload(query, 
+              method = "api", 
+              files.per.chunk = 20, 
+              directory = data_path)
+  
+  data <- GDCprepare(query,
+                     directory = data_path)
+  metadata <- as.data.frame(colData(data))
 
-# Download files (about 546MB of data)
-GDCdownload(query, 
-            method = "api", 
-            files.per.chunk = 20, 
-            directory = "C:/Users/Sarah/Desktop/Repos/GeneExpressionDimReduction/data")
+  # process columns containing lists
+  cleaned_data <- subset(metadata, select=-c(treatments, primary_site, disease_type))
 
-data <- GDCprepare(query,
-                   directory = "C:/Users/Sarah/Desktop/Repos/GeneExpressionDimReduction/data")
-metadata <- as.data.frame(colData(data))
-
-# process columns containing lists
-cleaned_data <- subset(metadata, select=-c(treatments, primary_site, disease_type))
-
-# write the metadata to CSV
-filename = "C:/Users/Sarah/Desktop/Repos/GeneExpressionDimReduction/metadata.csv"
-write_csv(
-  cleaned_data,
-  filename,
-  na = "NA",
-  append = FALSE,
-  col_names = TRUE,
-  quote_escape = "double",
-  eol = "\n"
-)
-
+  # write the metadata to CSV
+  filename = paste("C:/Users/Sarah/Desktop/Repos/GeneExpressionDimReduction/metadata/", project_name, ".csv", sep="")
+  write_csv(
+    cleaned_data,
+    filename,
+    na = "NA",
+    append = FALSE,
+    col_names = TRUE,
+    quote_escape = "double",
+    eol = "\n"
+  )
+}
