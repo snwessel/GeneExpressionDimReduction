592efd06bcad5f4ea40058559e3914434cd90183
snwessel <wessel.s@husky.neu.edu>
2021-04-09 12:49:30 -0400

    Update visualizations to use fully processed data

--- visualize.py
+++ visualize.py
@@ -7,7 +7,7 @@ num_points = 300
 
 # load gene expression data as a numpy array
 print("Loading data...")
-X = np.genfromtxt("data/processed-data/gene-expression.csv", delimiter=",")
+X = np.genfromtxt("data/train-test-data/X.csv", delimiter=",")
 print("X shape:", X.shape)
 
 # embed using t-SNE
@@ -21,14 +21,21 @@ fit = pca.fit(X.T)
 pca_components = fit.components_
 
 # load the diagnosis labels
-labels = np.genfromtxt("data/processed-data/ordered-diagnoses.csv", delimiter=",", dtype=str)
+labels = np.genfromtxt("data/train-test-data/y.csv", delimiter=",", dtype=int)
+label_name_dict = {
+  0: "Acute myeloid leukemia",
+  1: "Clear cell adenocarcinoma",
+  2: "Squamous cell carcinoma"
+}
+
 
 # create the plots
 fig, ax = plt.subplots()
 for label in np.unique(labels):
   print("graphing label", label)
   i = np.where(labels == label)
-  ax.scatter(X_embedded.T[0][i], X_embedded.T[1][i], label=label)
+  label_name = label_name_dict[label]
+  ax.scatter(X_embedded.T[0][i], X_embedded.T[1][i], label=label_name)
 ax.legend(title="Tumor Diagnosis")
 plt.title("t-SNE")
 plt.show()
@@ -36,7 +43,8 @@ plt.show()
 fig, ax = plt.subplots()
 for label in np.unique(labels):
   i = np.where(labels == label)
-  ax.scatter(pca_components[0][i], pca_components[1][i], label=label)
+  label_name = label_name_dict[label]
+  ax.scatter(pca_components[0][i], pca_components[1][i], label=label_name)
 ax.legend(title="Tumor Diagnosis")
 plt.title("PCA")
 plt.show()
