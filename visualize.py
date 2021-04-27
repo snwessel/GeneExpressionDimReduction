3cf4bbeb379399e7f3df9798dda8b34c53c1cd25
snwessel <wessel.s@husky.neu.edu>
2021-04-26 20:50:39 -0400

    Implement Kernel PCA visualizations

--- visualize.py
+++ visualize.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, KernelPCA
 from sklearn.manifold import TSNE
 
 num_points = 300
@@ -9,16 +9,6 @@ num_points = 300
 print("Loading data...")
 X = np.genfromtxt("data/train-test-data/X.csv", delimiter=",")
 print("X shape:", X.shape)
-
-# embed using t-SNE
-print("Reducing dimensions...")
-X_embedded = TSNE(n_components=2).fit_transform(X)
-print("Reduced from", X.shape, "to", X_embedded.shape)
-
-# embed using PCA
-pca = PCA(n_components = 2)
-fit = pca.fit(X.T)
-pca_components = fit.components_
 
 # load the diagnosis labels
 labels = np.genfromtxt("data/train-test-data/y.csv", delimiter=",", dtype=int)
@@ -29,10 +19,13 @@ label_name_dict = {
 }
 
 
-# create the plots
+# embed using t-SNE
+print("Reducing dimensions...")
+X_embedded = TSNE(n_components=2).fit_transform(X)
+print("Reduced from", X.shape, "to", X_embedded.shape)
+
 fig, ax = plt.subplots()
 for label in np.unique(labels):
-  print("graphing label", label)
   i = np.where(labels == label)
   label_name = label_name_dict[label]
   ax.scatter(X_embedded.T[0][i], X_embedded.T[1][i], label=label_name)
@@ -40,11 +33,19 @@ ax.legend(title="Tumor Diagnosis")
 plt.title("t-SNE")
 plt.show()
 
-fig, ax = plt.subplots()
-for label in np.unique(labels):
-  i = np.where(labels == label)
-  label_name = label_name_dict[label]
-  ax.scatter(pca_components[0][i], pca_components[1][i], label=label_name)
-ax.legend(title="Tumor Diagnosis")
-plt.title("PCA")
-plt.show()
+# embed using PCA
+kernels = ["linear", "poly", "rbf", "sigmoid", "cosine", "precomputed"]
+for kernel in kernels:
+  print("Running PCA with the kernel", kernel)
+  pca = KernelPCA(n_components = 2)
+  fit = pca.fit(X.T)
+  pca_components = fit.components_
+
+  fig, ax = plt.subplots()
+  for label in np.unique(labels):
+    i = np.where(labels == label)
+    label_name = label_name_dict[label]
+    ax.scatter(pca_components[0][i], pca_components[1][i], label=label_name)
+  ax.legend(title="Tumor Diagnosis")
+  plt.title("PCA with " + kernel + "kernel")
+  plt.show()
